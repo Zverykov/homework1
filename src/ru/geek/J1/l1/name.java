6f3c978849b286c6735bc146986a4f17e3c66c80
Andrew <pivovar777@yandex.ru>
2022-01-08 22:33:31 +0300

    1

--- src/ru/geek/J1/l1/name.java
+++ src/ru/geek/J1/l1/name.java
@@ -1,2 +1,19 @@
-package ru.geek.J1.l1;public class name {
+package ru.geek.J1.l1;
+
+public class name {
+
+    private static String imya(String a){
+        String b = "Привет," + a + "!";
+        return (b);
+    }
+
+    public static void main(String[] arg){
+     String a = "1";
+     String c = imya(a);
+      System.out.println(c);
+
+    }
+
+
+
 }
--- src/ru/geek/J1/l1/year.java
+++ src/ru/geek/J1/l1/year.java
@@ -1,2 +1,27 @@
-package ru.geek.J1.l1;public class year {
-}
+package ru.geek.J1.l1;
+
+import java.util.Scanner;
+
+
+public class year {
+
+    private static String god (int x) {
+        String b;
+        if (x % 4 == 0 && x % 100 > 0){
+            b = "Весокостный год";
+        }
+        else{
+            b = "Не весокостный год";
+        }
+        return (b);
+    }
+        public static void main (String[]arg){
+
+int x = 1604;
+
+
+
+ String a = god(x);
+ System.out.println(a);
+        }
+    }
